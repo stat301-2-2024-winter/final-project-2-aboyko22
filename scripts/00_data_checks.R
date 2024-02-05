9ae18b4c53642d1bfa3a74ae09b1f78ad901024d
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-02-04 19:17:34 -0600

    second idea: no

--- scripts/00_data_checks.R
+++ scripts/00_data_checks.R
@@ -1,6 +1,6 @@
 # load packages ----
 library(tidyverse)
-library(nflfastR)
+library(nflverse)
 
 data2023 <- load_pbp(2023) %>%
   filter(season_type == "REG")
@@ -9,3 +9,4 @@ data2023 %>%
   ggplot(aes(x = wpa, fill = play_type)) +
   geom_histogram(bins = 100) +
   facet_wrap(~play_type)
+
