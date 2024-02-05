2f493b7cda851d96eb8e4cf44edec6380648e2da
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-02-04 18:18:59 -0600

    initial idea

---
+++ scripts/00_data_checks.R
@@ -0,0 +1,11 @@
+# load packages ----
+library(tidyverse)
+library(nflfastR)
+
+data2023 <- load_pbp(2023) %>%
+  filter(season_type == "REG")
+
+data2023 %>%
+  ggplot(aes(x = wpa, fill = play_type)) +
+  geom_histogram(bins = 100) +
+  facet_wrap(~play_type)
