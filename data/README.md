3b4a1f4677ea488350c6902c41aa851c88782f40
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-02-04 23:11:01 -0600

    progress memo 1

--- data/README.md
+++ data/README.md
@@ -1,5 +1,5 @@
 ## Datasets
 
-Cite and describe datasets ....
+All data is coming from `nflverse` package. Currently, this subdirectory holds only the raw data file, which was loaded into an R script and saved out immediately. You can learn more about the package [here](tps://nflverse.nflverse.com).
 
 
--- scripts/00_data_checks.R
+++ scripts/00_data_checks.R
@@ -1,12 +1,27 @@
 # load packages ----
 library(tidyverse)
 library(nflverse)
+library(scales)
+library(here)
 
-data2023 <- load_pbp(2023) %>%
+data_2023 <- load_pbp(2023) %>%
   filter(season_type == "REG")
 
-data2023 %>%
-  ggplot(aes(x = wpa, fill = play_type)) +
-  geom_histogram(bins = 100) +
-  facet_wrap(~play_type)
+write_csv(data_2023, file = here("data/raw_data.csv"))
 
+# data selection
+modeling_data <- data_2023 %>%
+  filter(between(week, 2, 17),
+         !is.na(down),
+         play_type %in% c("pass", "run"))
+
+modeling_data %>% count(play_type)
+
+modeling_data %>%
+  ggplot(aes(x = play_type, fill = play_type)) +
+  geom_bar() +
+  labs(x = "Play Type", y = "Count") +
+  theme(legend.position = "none") +
+  scale_y_continuous(labels = comma)
+
+ggsave(filename = "play_type.jpg", path = here("plots/"))
