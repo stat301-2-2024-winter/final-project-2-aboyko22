20b05c9621b7514b4ade4300b0bfeb00483b0e9c
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-02-29 00:06:49 -0600

    progress memo 2

--- scripts/03_baseline.R
+++ scripts/03_baseline.R
@@ -26,11 +26,27 @@ null_wflow <- workflow() %>%
 
 null_fit <- fit_resamples(null_wflow, data_folds, control = control_resamples(save_workflow = FALSE))
 
-null_fit %>%
-  collect_metrics(metric = "accuracy")
-
 # Defining Basic Recipe
-basic_spec <-
+basic_spec <- logistic_reg(penalty = 0.01) %>%
+  set_engine('glmnet') %>%
+  set_mode("classification")
+
+basic_wflow <- workflow() %>%
+  add_recipe(basic_recipe) %>%
+  add_model(basic_spec)
+
+basic_fit <- fit_resamples(basic_wflow, data_folds, control = control_resamples(save_workflow = FALSE))
+
+id <- tibble(id = c('null', "basic"))
+null_results <- collect_metrics(null_fit) %>% select(.metric, mean, n, std_err)      
+basic_results <- collect_metrics(basic_fit) %>% select(.metric, mean, n, std_err)  
+
+baseline_results <- rbind(null_results, basic_results) %>%
+  filter(.metric == "accuracy") %>%
+  bind_cols(id) %>% relocate(id)
+  
+
+save(baseline_results, file = here("memos/memo_files/baseline_results.rda"))
 
 
 # To Do List ----
