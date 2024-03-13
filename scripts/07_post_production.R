6854ddbac9d2134b794485583c8a2c2d6471f56c
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-03-13 01:32:05 -0400

    qmd bonus fit

--- scripts/07_post_production.R
+++ scripts/07_post_production.R
@@ -13,6 +13,8 @@ registerDoMC(cores = 6)
 
 # load data ----
 load(here("data/cleaned_data/cleaned_data.rda"))
+load(here("results/final_fit.rda"))
+load(here("recipes/full_recipe.rda"))
 
 # Identifying Example Team
 modeling_data %>%
@@ -31,5 +33,40 @@ modeling_data %>%
 texans_data <- modeling_data %>%
   filter(posteam == "HOU")
 
+set.seed(12125)
+texans_split <- initial_split(texans_data, prop = 0.75, strata = play_type)
 
-# To Do List ----
+texans_training <- training(texans_split)
+texans_testing <- testing(texans_split)
+
+get_metrics <- metric_set(accuracy)
+
+# texans rf forest
+rf_spec <- rand_forest(mtry = 8, min_n = 20, trees = 1500) %>%
+  set_engine("ranger") %>%
+  set_mode("classification")
+
+full_rf <- workflow() %>%
+  add_model(rf_spec) %>%
+  add_recipe(full_recipe)
+
+# fit to testing
+set.seed(31046)
+texans_fit <- full_rf %>% fit(texans_training)
+
+# compare
+texans_predictions <- bind_cols(predict(texans_fit, texans_testing), texans_testing)
+save(texans_predictions, file = here("results/texans_results.rda"))
+
+
+get_metrics(texans_predictions, truth = play_type, estimate = .pred_class)
+
+
+
+
+
+
+
+
+
+
